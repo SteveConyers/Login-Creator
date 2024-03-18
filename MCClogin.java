1ce9564bfb7884172c709bba677e0310aa731971
Steven Conyers <140514213+SteveConyers@users.noreply.github.com>
2024-03-18 15:49:48 -0400

    Update MCClogin.java
    Removed program description. Added created and modified date.

--- MCClogin.java
+++ MCClogin.java
@@ -1,61 +1,58 @@
 package MCClogin;
 
-// MCClogin.java by Steven Conyers - 3/8/2022
-
-/* This program demonstrates the StringBuilder class along with StringBuilder objects.
-   It gets user input from keyboard from three different strings.
-   It creates a user name by combining the first to inputs from the user using
-   string builder append methods.
-   Another output is for password from user input
-   Program then outputs both created string in a single display message*/
+/* MCClogin.java by Steven Conyers
+   Created: 3/8/2022
+   Edited: 3/18/2024 */
 
 import javax.swing.JOptionPane;
 
-public class MCClogin {
-	public static void main(String[] args) {
-		// Declarations
-		String first;
-		String last;
-		String stuNum;
+public class MCClogin
+{
+	public static void main(String[] args)
+	{
+	// Declarations
+	  String first;
+	  String last;
+	  String stuNum;
 
-		// Get user input
-		first = JOptionPane.showInputDialog("Please enter your first name:");
+	  // Get user input
+	  first = JOptionPane.showInputDialog("Please enter your first name:");
 
-		last = JOptionPane.showInputDialog("Please enter your last name:");
+	  last = JOptionPane.showInputDialog("Please enter your last name:");
 
-		stuNum = JOptionPane.showInputDialog("Please enter in your student number:");
+	  stuNum = JOptionPane.showInputDialog("Please enter in your student number:");
 
-		// StringBuilder for user name
-		StringBuilder userName = new StringBuilder();
+	  // StringBuilder for user name
+	  StringBuilder userName = new StringBuilder();
 
-		userName.append(first.toLowerCase());
-		userName.append(".");
-		userName.append(last.toLowerCase());
+	  userName.append(first.toLowerCase());
+	  userName.append(".");
+	  userName.append(last.toLowerCase());
 
-		// StringBuilder for password
-		StringBuilder password = new StringBuilder();
+	  // StringBuilder for password
+	  StringBuilder password = new StringBuilder();
 
-		password.insert(0, userName.charAt(0));
-		password.insert(1, last.charAt(0));
-		password.append(stuNum);
-		password.append("!");
+	  password.insert(0, userName.charAt(0));
+	  password.insert(1, last.charAt(0));
+	  password.append(stuNum);
+	  password.append("!");
 
-		// StringBuilder for message
-		StringBuilder message = new StringBuilder();
+	  // StringBuilder for message
+	  StringBuilder message = new StringBuilder();
 
-		message.append("Your user name is ");
-		message.append(userName);
-		message.append(" and your password is ");
-		message.append(password);
+	  message.append("Your user name is ");
+	  message.append(userName);
+	  message.append(" and your password is ");
+	  message.append(password);
 
-		// Output messages
-		JOptionPane.showMessageDialog(null, "userName - " + userName);
+	  // Output messages
+	  JOptionPane.showMessageDialog(null, "userName - " + userName);
 
-		JOptionPane.showMessageDialog(null, "password - " + password);
+	  JOptionPane.showMessageDialog(null, "password - " + password);
 
-		JOptionPane.showMessageDialog(null, message);
+	  JOptionPane.showMessageDialog(null, message);
 
-		// Close program
-		System.exit(0);
+	  // Close program
+	  System.exit(0);
 	}
 }
